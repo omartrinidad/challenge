218c6f452f1081d463f0d822400ce25e1c4cfc3d
Omar Gutiérrez <omar.vpa@gmail.com>
2017-04-17 01:33:50 +0200

    Analysis of age and hear song

---
+++ eda.py
@@ -0,0 +1,35 @@
+import pandas as pd
+import numpy as np
+import matplotlib
+from matplotlib import pyplot as plt
+plt.style.use('seaborn-deep')
+
+
+################################################################################
+# Exploratory Data Analysis
+# columns list(small)
+# sample.drop('is_listened', 1)
+################################################################################
+
+# read the serialized sample
+sample = pd.read_pickle('data/second.dsg')
+
+# relationship between age and skip song
+sample = sample[['user_age', 'is_listened']]
+ages = sample['user_age'].unique()
+ages.sort()
+table = sample.groupby(['user_age', 'is_listened']).size()
+table = table.sort_index(level='is_listened')
+
+result = table.as_matrix()
+
+plt.bar(ages, result[13:], width=0.5, color='g', align='center')
+plt.bar(ages+0.5, result[:13], width=0.5, color='r', align='center')
+plt.set_yticklabels(ages, minor=False)
+plt.legend(('Is Listened', 'Not Listened'), loc='upper right')
+
+# According to age how much the people skip the song?
+# yes = sample.loc[sample['is_listened'] == 1]
+# no = sample.loc[sample['is_listened'] == 0]
+
+plt.show()

03cb482939e0c3f9a0a7c1cb875f57ea57a85a88
Omar Gutiérrez <omar.vpa@gmail.com>
2017-04-20 21:28:11 +0200

    More preprocessing

--- eda.py
+++ eda.py
@@ -1,34 +1,68 @@
+################################################################################
+# Exploratory Data Analysis
+# columns list(sample)
+# sample.drop('is_listened', 1)
+# yes = sample.loc[sample['is_listened'] == 1]
+# no = sample.loc[sample['is_listened'] == 0]
+################################################################################
+
 import pandas as pd
 import numpy as np
 import matplotlib
 from matplotlib import pyplot as plt
 plt.style.use('seaborn-deep')
 
-
-################################################################################
-# Exploratory Data Analysis
-# columns list(small)
-# sample.drop('is_listened', 1)
-################################################################################
-
 # read the serialized sample
-sample = pd.read_pickle('data/second.dsg')
+sample = pd.read_pickle('data/first_p.dsg')
 
-# relationship between age and skip song
-sample = sample[['user_age', 'is_listened']]
-ages = sample['user_age'].unique()
-ages.sort()
-table = sample.groupby(['user_age', 'is_listened']).size()
-table = table.sort_index(level='is_listened')
 
-result = table.as_matrix()
+def age_skip_song():
+    # relationship between age and skip song
+    age_listen = sample[['user_age', 'is_listened']]
+    ages = age_listen['user_age'].unique()
+    ages.sort_values()
+    table = age_listen.groupby(['user_age', 'is_listened']).size()
+    table = table.sort_index(level='is_listened')
+    result = table.as_matrix()
 
-plt.bar(ages, result[13:], width=0.5, color='g', align='center')
-plt.bar(ages+0.5, result[:13], width=0.5, color='r', align='center')
-plt.legend(('Is Listened', 'Not Listened'), loc='upper right')
+    plt.bar(ages, result[13:], width=0.5, color='g', align='center')
+    plt.bar(ages+0.5, result[:13], width=0.5, color='r', align='center')
+    plt.legend(('Is Listened', 'Not Listened'), loc='upper right')
+    plt.show()
 
-# According to age how much the people skip the song?
-# yes = sample.loc[sample['is_listened'] == 1]
-# no = sample.loc[sample['is_listened'] == 0]
 
-plt.show()
+def times():
+    # At which time the people hear the songs
+    hour = sample[['hour']]
+    hours = hour['hour'].unique()
+    hours.sort()
+    table = hour.groupby(['hour']).size()
+    table = table.sort_index(level='hour')
+    result = table.as_matrix()
+
+    plt.bar(hours, result, width=0.5, color='g', align='center')
+    plt.legend("People hearing music", loc='upper right')
+    plt.xlim(0, 24)
+    plt.show()
+
+
+def histogram(sample, column_name):
+    """Create a histogram"""
+    column = sample[[column_name]]
+    values = column[column_name].unique()
+    values.sort()
+    #print(values)
+    table = column.groupby([column_name]).size()
+    table = table.sort_index(level=column_name)
+    #print(table)
+    result = table.as_matrix()
+
+    plt.bar(values, result, width=0.5, color='g', align='center')
+    plt.legend("Title", loc='upper right')
+    # plt.xlim(values[0], values[-1])
+    plt.show()
+    
+
+# histogram(sample, 'release_year')
+# times()
+# age_skip_song()
--- preprocess_ts.py
+++ preprocess_ts.py
@@ -1,16 +1,61 @@
 import pandas as pd
 import numpy as np
-import datetime
+from datetime import datetime
+fromtimestamp = datetime.fromtimestamp
 
 
-df = pd.read_csv("data/train.csv")
+def sampling(name_file):
+    """
+    From original dataset recovers a sample and save this as serialized file
+    After data is saved we read it in the next way:
+    sample = pd.read_pickle(name_file)
+    """
+    dataset = pd.read_csv("data/train.csv")
+    sample = dataset.sample(100000)
+    sample.to_pickle(name_file)
 
-df['year'] = df['ts_listen'].apply(lambda x: datetime.datetime.fromtimestamp(x).year)
-df['month'] = df['ts_listen'].apply(lambda x: datetime.datetime.fromtimestamp(x).month)
-df['day'] = df['ts_listen'].apply(lambda x: datetime.datetime.fromtimestamp(x).day)
-df['hour'] = df['ts_listen'].apply(lambda x: datetime.datetime.fromtimestamp(x).hour)
-df['minute'] = df['ts_listen'].apply(lambda x: datetime.datetime.fromtimestamp(x).minute)
-df['weekday'] = df['ts_listen'].apply(lambda x: datetime.datetime.fromtimestamp(x).isoweekday())
 
-df.to_csv("data/train_p.csv")
+def times2categorical(dataset):
+    """Converts timestamp columns to categorical columns"""
+    dataset['year'] = dataset['ts_listen'].apply(lambda x: fromtimestamp(x).year)
+    dataset['month'] = dataset['ts_listen'].apply(lambda x: fromtimestamp(x).month)
+    dataset['day'] = dataset['ts_listen'].apply(lambda x: fromtimestamp(x).day)
+    dataset['hour'] = dataset['ts_listen'].apply(lambda x: fromtimestamp(x).hour)
+    dataset['minute'] = dataset['ts_listen'].apply(lambda x: fromtimestamp(x).minute)
+    dataset['weekday'] = dataset['ts_listen'].apply(lambda x: fromtimestamp(x).isoweekday())
+    return dataset
 
+
+def classify_age(age):
+    """"""
+    cat = ""
+    if age <= 19:
+        cat = "teen"
+    elif age >= 26 and age <= 25:
+        cat = "young"
+    elif age >= 26 and age <= 29:
+        cat = "adult"
+    else:
+        cat = "thirty_guy"
+    return cat
+
+
+def ages2categorical(dataset):
+    """converts age column to categorical values:
+    """
+    dataset['ages_cat'] = dataset['user_age'].apply(lambda x: classify_age(x))
+    return dataset
+
+
+def release2categorical(dataset):
+    """Converts release column to categorical values:
+    80's, 90's, 2000's, 2010's
+    """
+    dataset['release_year'] = dataset['release_date'].apply(lambda x: fromtimestamp(x).year)
+    return dataset
+
+
+# already done
+# df = pd.read_csv("data/train.csv")
+# times2categorical(df)
+# df.to_csv("data/train_p.csv")
